fdfa3bb0a730b06205fc66c3ad6d70593ffb8c9a
Abdelrahman <abdokhm13@gmail.com>
2025-12-18 17:29:39 +0200

    Seed Initial data in the database

--- Server/Clinic/Clinic.DataAccess/Data/AppDbContext.cs
+++ Server/Clinic/Clinic.DataAccess/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Clinic.Models;
+using Clinic.Models.Enums;
 using Microsoft.EntityFrameworkCore;
 
 namespace Clinic.DataAccess.Data
@@ -56,7 +57,182 @@ namespace Clinic.DataAccess.Data
             modelBuilder.Entity<ApplicationUser>().HasIndex(u => u.Username).IsUnique();
             modelBuilder.Entity<ApplicationUser>().HasIndex(u => u.Email).IsUnique();
 
+            LoadUsers(modelBuilder);
+            LoadPatients(modelBuilder);
+            LoadDoctors(modelBuilder);
+            LoadSpecialties(modelBuilder);
+            LoadDoctorSpecialties(modelBuilder);
+            LoadAppointments(modelBuilder);
+            LoadPayments(modelBuilder);
+
             base.OnModelCreating(modelBuilder);
         }
+
+        private static void LoadUsers(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ApplicationUser>().HasData(
+                new ApplicationUser
+                {
+                    Id = 1,
+                    Username = "admin",
+                    Email = "admin@clinic.com",
+                    PasswordHash = "admin-hash",
+                    Role = Role.Admin
+                },
+                new ApplicationUser
+                {
+                    Id = 2,
+                    Username = "abdelrahman",
+                    Email = "abdelrahman@clinic.com",
+                    PasswordHash = "123456",
+                    Role = Role.Doctor
+                },
+                new ApplicationUser
+                {
+                    Id = 3,
+                    Username = "ahmed",
+                    Email = "ahmed@clinic.com",
+                    PasswordHash = "123456",
+                    Role = Role.Patient
+                },
+                 new ApplicationUser
+                 {
+                     Id = 4,
+                     Username = "amr",
+                     Email = "amr@clinic.com",
+                     PasswordHash = "123456",
+                     Role = Role.Doctor
+                 },
+                 new ApplicationUser
+                 {
+                     Id = 5,
+                     Username = "samy",
+                     Email = "samy@clinic.com",
+                     PasswordHash = "123456",
+                     Role = Role.Doctor
+                 }
+            );
+        }
+
+        private static void LoadPatients(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>().HasData(
+                new Patient
+                {
+                    Id = 1,
+                    UserId = 3,
+                    FullName = "Samy Mohamed",
+                    PhoneNumber = "01123456789",
+                    Balance = 3700m
+                },
+                new Patient
+                {
+                    Id = 2,
+                    UserId = 5,
+                    FullName = "Ahmed Ali",
+                    PhoneNumber = "01123856109",
+                    Balance = 2500m
+                }
+            );
+        }
+
+        private static void LoadDoctors(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Doctor>().HasData(
+                new Doctor
+                {
+                    Id = 1,
+                    UserId = 2,
+                    FullName = "Abdelrahman Khaled",
+                    Biography = "Senior Cardiologist"
+                },
+                new Doctor
+                {
+                    Id = 2,
+                    UserId = 4,
+                    FullName = "Amr Sobhy",
+                    Biography = "Senior Dermatologist"
+                }
+            );
+        }
+
+        private static void LoadSpecialties(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Specialty>().HasData(
+                new Specialty { Id = 1, Name = "Cardiology" },
+                new Specialty { Id = 2, Name = "Dermatology" },
+                new Specialty { Id = 3, Name = "Neurology" }
+            );
+        }
+
+        private static void LoadDoctorSpecialties(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DoctorSpecialty>().HasData(
+                new DoctorSpecialty
+                {
+                    DoctorId = 1,
+                    SpecialtyId = 1
+                },
+                new DoctorSpecialty
+                {
+                    DoctorId = 2,
+                    SpecialtyId = 2
+                }
+            );
+        }
+
+        private static void LoadAppointments(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Appointment>().HasData(
+                new Appointment
+                {
+                    Id = 1,
+                    PatientId = 1,
+                    DoctorId = 1,
+                    StartTime = new DateTime(2025, 1, 20, 10, 0, 0),
+                    EndTime = new DateTime(2025, 1, 20, 10, 30, 0),
+                    Status = AppointmentStatus.Pending,
+                    AppointmentType = AppointmentType.Regular,
+                    Fee = 300m,
+                    Notes = "Initial checkup"
+                },
+                new Appointment
+                {
+                    Id = 2,
+                    PatientId = 2,
+                    DoctorId = 2,
+                    StartTime = new DateTime(2025, 5, 17, 8, 0, 0),
+                    EndTime = new DateTime(2025, 5, 17, 8, 30, 0),
+                    Status = AppointmentStatus.Pending,
+                    AppointmentType = AppointmentType.FollowUp,
+                    Fee = 100m,
+                    Notes = "Follow Up Appointment"
+                }
+            );
+        }
+
+        private static void LoadPayments(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Payment>().HasData(
+                new Payment
+                {
+                    Id = 1,
+                    AppointmentId = 1,
+                    Amount = 300m,
+                    PaymentMethod = PaymentMethod.Cash,
+                    PaidAt = new DateTime(2025, 1, 20, 9, 55, 0)
+                },
+                new Payment
+                {
+                    Id = 2,
+                    AppointmentId = 2,
+                    Amount = 100m,
+                    PaymentMethod = PaymentMethod.CreditCard,
+                    PaidAt = new DateTime(2025, 5, 15, 6, 40, 0)
+                }
+            );
+        }
+
+
     }
 }
---
+++ Server/Clinic/Clinic.DataAccess/Data/Migrations/20251218152846_SeedInitialData.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Clinic.DataAccess.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Specialties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Cardiology" },
+                    { 2, "Dermatology" },
+                    { 3, "Neurology" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "PasswordHash", "Role", "Username" },
+                values: new object[,]
+                {
+                    { 1, "admin@clinic.com", "admin-hash", 2, "admin" },
+                    { 2, "abdelrahman@clinic.com", "123456", 1, "abdelrahman" },
+                    { 3, "ahmed@clinic.com", "123456", 0, "ahmed" },
+                    { 4, "amr@clinic.com", "123456", 1, "amr" },
+                    { 5, "samy@clinic.com", "123456", 1, "samy" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Doctors",
+                columns: new[] { "Id", "Biography", "FullName", "UserId" },
+                values: new object[,]
+                {
+                    { 1, "Senior Cardiologist", "Abdelrahman Khaled", 2 },
+                    { 2, "Senior Dermatologist", "Amr Sobhy", 4 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Patients",
+                columns: new[] { "Id", "Balance", "FullName", "PhoneNumber", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 3700m, "Samy Mohamed", "01123456789", 3 },
+                    { 2, 2500m, "Ahmed Ali", "01123856109", 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Appointments",
+                columns: new[] { "Id", "AppointmentType", "DoctorId", "EndTime", "Fee", "Notes", "PatientId", "StartTime", "Status" },
+                values: new object[,]
+                {
+                    { 1, 0, 1, new DateTime(2025, 1, 20, 10, 30, 0, 0, DateTimeKind.Unspecified), 300m, "Initial checkup", 1, new DateTime(2025, 1, 20, 10, 0, 0, 0, DateTimeKind.Unspecified), 0 },
+                    { 2, 2, 2, new DateTime(2025, 5, 17, 8, 30, 0, 0, DateTimeKind.Unspecified), 100m, "Follow Up Appointment", 2, new DateTime(2025, 5, 17, 8, 0, 0, 0, DateTimeKind.Unspecified), 0 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "DoctorSpecialties",
+                columns: new[] { "DoctorId", "SpecialtyId", "SpecialtyId1" },
+                values: new object[,]
+                {
+                    { 1, 1, null },
+                    { 2, 2, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Payments",
+                columns: new[] { "Id", "Amount", "AppointmentId", "PaidAt", "PatientId", "PaymentMethod" },
+                values: new object[,]
+                {
+                    { 1, 300m, 1, new DateTime(2025, 1, 20, 9, 55, 0, 0, DateTimeKind.Unspecified), null, 0 },
+                    { 2, 100m, 2, new DateTime(2025, 5, 15, 6, 40, 0, 0, DateTimeKind.Unspecified), null, 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DoctorSpecialties",
+                keyColumns: new[] { "DoctorId", "SpecialtyId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "DoctorSpecialties",
+                keyColumns: new[] { "DoctorId", "SpecialtyId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Payments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Payments",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Specialties",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Appointments",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Specialties",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Specialties",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Patients",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
